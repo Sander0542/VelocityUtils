53565f0562890f21a01d4c588978c6a2d91784d9
Sander Jochems <sanderjochems@hotmail.nl>
2021-07-08 17:56:47 +0200

    Update Constants.java

--- src/main/java/com/sanderjochems/minecraft/velocityutils/Constants.java
+++ src/main/java/com/sanderjochems/minecraft/velocityutils/Constants.java
@@ -3,4 +3,8 @@ package com.sanderjochems.minecraft.velocityutils;
 public class Constants {
     public static final String PluginId = "velocity-utils";
     public static final String PluginName = "VelocityUtils";
+
+    public static final int BStatsPluginId = 11571;
+
+    public static final String VersionApi = "https://api.github.com/repos/Sander0542/VelocityUtils/releases/latest";
 }
